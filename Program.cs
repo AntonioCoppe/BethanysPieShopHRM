fdcab41f4ec9e3030d1fa48b47ecf2aed0c1b4ba
Antonio Coppe <antonio.coppe@gmail.com>
2022-11-08 10:40:55 +0100

    Finished Current Module

--- Program.cs
+++ Program.cs
@@ -11,11 +11,51 @@ namespace BethanysPieShop
 
             bool isActive = true;
             double rating = 99.25;
-            //byte numberOfEmployees = 300;
+            
+            double ratePerHour = 12.34;
+            int numberOfHoursWorked = 165;
 
             
 
-            Console.ReadLine();
+            long veryLongMonth = numberOfHoursWorked;
+            double d = 123456789.0;
+            int x = (int)d;
+
+            int intVeryLongMonth = (int)veryLongMonth;
+
+            double currentMonthWage = ratePerHour * numberOfHoursWorked + bonus;
+
+            Console.WriteLine(currentMonthWage);
+
+            ratePerHour += 3; 
+
+            if (currentMonthWage > 2000)
+            {
+                Console.WriteLine("Top paid Employee");
+            }
+            int numberOfEmployees = 15;
+            numberOfEmployees --;
+
+            int intMaxValue = int.MaxValue;
+            int intMinValue = int.MinValue;
+
+            char userSelection = 'a';
+            char upperVersion = char.ToUpper(userSelection);
+            bool isDigit = char.IsDigit(userSelection);
+            bool isLetter = char.IsLetter(userSelection);
+
+            DateTime hireDate = new DateTime(2021,3, 28, 14, 30, 0);
+
+            DateTime startDate = hireDate.AddDays(15);
+            DateTime currentDate = DateTime.Now;
+
+            bool areWeInDst = currentDate.IsDaylightSavingTime();
+
+            DateTime startHour = DateTime.Now;
+            TimeSpan worktime = new TimeSpan(8, 35, 0);
+            DateTime endHour = startHour.Add(worktime);
+
+            Console.WriteLine(hireDate);
         }
     }
 }
