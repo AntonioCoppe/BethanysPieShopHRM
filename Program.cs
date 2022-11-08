619bd6573f766040086d267007988e5c1086d0d4
Antonio Coppe <antonio.coppe@gmail.com>
2022-11-08 09:26:58 +0100

    First Commit

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace BethanysPieShop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int monthlyWage = 1234;
+            int months = 12, bonus = 1000;
+
+            bool isActive = true;
+            double rating = 99.25;
+            //byte numberOfEmployees = 300;
+
+            
+
+            Console.ReadLine();
+        }
+    }
+}
